6c4142e7231a3d62bd111e209205ade6ce306820
Federico Nuñez <nunez.fedema@gmail.com>
2023-08-23 21:28:22 -0300

    entraga cargada

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+export const config = {
+    server:{
+        port:8080
+    },
+    fileSystem:{
+        productsFile:"products.json",
+        cartFile:"carts.json"
+    },
+    mongo:{
+        url:"mongodb+srv://vkycardoso:<password>@cluster0.m0yuyf2.mongodb.net/?retryWrites=true&w=majority"
+    }
+}
